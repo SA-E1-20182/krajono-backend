e58710cc41071f84053c8b1e9bf513e526c23990
fffeelipe <lfepiar@unal.edu.co>
2019-02-08 09:11:37 -0500

    Fuck rancher/server, and comment many stuff

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 ./rancher-compose \
 --project-name krajono_backend \
 --url http://35.190.145.103:8080/v1/projects/1a5 \
---access-key 7E4C9C8BC4D715B32623 \
---secret-key Rw4MCbbwUfR92uPtf3HMsthvABNnpXg71cLf9bBG \
+--access-key 838130F594F18E78E662 \
+--secret-key xFv2WZyv1rcsM1CU2RaXu1vgowLxEJqXQRNd2bYh \
 -f docker-compose.yml \
 --verbose up \
 -d --force-upgrade \
