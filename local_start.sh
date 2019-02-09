12e4aff1ce176b18e1014f0d24dbe931ea08d11a
fffeelipe <lfepiar@unal.edu.co>
2019-02-09 17:12:25 -0500

    hosts with subnet

--- local_start.sh
+++ local_start.sh
@@ -1,8 +1,8 @@
 ./rancher-compose \
 --project-name krajono \
 --url http://192.168.99.100:8080/v1/projects/1a5 \
---access-key ED2F18F3E755A1A54CCD \
---secret-key fZvTzHRHuub2kXxWqz6PyNNxGQ5rff3Hn1Ucnkuw \
+--access-key EA8BF25F7B5B8FAFBD5D \
+--secret-key dPmnUaAv8Qzpmm918YZ6dBhe5kkJSRb4hNP5dNAy \
 -f docker-compose.yml \
 --verbose up \
 -d  \
